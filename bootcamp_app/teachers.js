4387212ff960b949a22ad6722a95ee78da2ecad8
Cedric.gauthier <cidgaut@gmail.com>
2024-02-06 07:16:14 -0500

    inluded parameterized text for user inputs while making sql queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,19 +7,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(*) AS total_assistances
-FROM assistance_requests
-JOIN teachers ON assistance_requests.teacher_id = teachers.id
-JOIN students ON assistance_requests.student_id = students.id
-JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
-GROUP BY teachers.id, cohorts.id
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.teacher} assisted in the ${user.cohort} cohort, with a total of ${user.total_assistances} assistances.`);
+const queryString = `
+  SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+  FROM assistance_requests
+  JOIN teachers ON assistance_requests.teacher_id = teachers.id
+  JOIN students ON assistance_requests.student_id = students.id
+  JOIN cohorts ON students.cohort_id = cohorts.id
+  WHERE cohorts.name = $1
+  ORDER BY teacher;
+`;
+
+const cohortName = process.argv[2];
+const values = [cohortName];
+
+pool.query(queryString, values)
+  .then(res => {
+    console.log("connected");
+    res.rows.forEach(entry => {
+      console.log(`${entry.cohort}: ${entry.teacher}`);
+    });
   })
-})
-.catch(err => console.error('query error', err.stack));
+  .catch(err => console.error('query error', err.stack))
+  .finally(() => pool.end());
